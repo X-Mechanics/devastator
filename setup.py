01eb16405ef958da6672463cbc2b79dcce085742
Michael Park <q1kollab@gmail.com>
2021-06-09 21:04:09 -0400

    added setup file

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+import os
+
+thelibFolder = os.path.dirname(os.path.realpath(__file__))
+requirementPath = thelibFolder + '/requirements.txt'
+
+install_requires = []
+if os.path.isfile(requirementPath):
+    with open(requirementPath) as f:
+        install_requires = f.read().splitlines()
+
+setup(
+    name='devastator', 
+    install_requires=install_requires,
+    version='0.0.0',
+    packages=find_packages()
+)
